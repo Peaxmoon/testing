83303ffa6974c4162fe6b67983f48f562c16fba0
Peaxmoon <sujjalboi09@gmail.com>
2023-08-09 14:42:56 +0545

    The website added

---
+++ new.js
@@ -0,0 +1,8 @@
+/*var video = document.getElementById('bg-video');
+var playButton = document.getElementById('play-button');
+
+playButton.addEventListener('click', function () {
+    video.play();
+    video.setAttribute('controls', 'controls'); // Show video controls
+    playButton.style.display = 'none'; // Hide the play button
+});*/
